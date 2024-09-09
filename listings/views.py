e487512f27abff7338f82914c153dd70fb4e8300
jessicapun <tojphappy@gmail.com>
2024-09-09 17:07:11 +0800

    Step 13 update  about

--- listings/views.py
+++ listings/views.py
@@ -4,12 +4,12 @@ from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 # Create your views here.
 def index(request):
-
-    listings = Listing.objects.all()
+# @ get all data from listing database
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-
+# @ pass database records into listings context
     context = {'listings': paged_listings}
     return render(request,'listings/listings.html', context)
 
